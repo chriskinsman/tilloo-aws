3760fded7243191fa4316ad2d77a967c1daa54dc
Chris Kinsman <chris@kinsman.net>
2019-09-30 10:05:11 -0700

    Update tilloo version

--- 6.build-container.sh
+++ 6.build-container.sh
@@ -1,3 +1,3 @@
 $(aws ecr get-login --no-include-email --profile $AWS_PROFILE)
-docker build -t $AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com/tilloo:4 .
-docker push $AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com/tilloo:4
+docker build -t $AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com/tilloo:5 .
+docker push $AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com/tilloo:5
