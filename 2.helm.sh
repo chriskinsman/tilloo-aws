f5194c5bb7b3d29dec5f6d714b8ae686abb26ea3
Chris Kinsman <chris@kinsman.net>
2019-09-30 09:57:17 -0700

    Helm wait for init

--- 2.helm.sh
+++ 2.helm.sh
@@ -1,6 +1,5 @@
-helm init
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-
+#kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+helm init --upgrade --wait --service-account tiller
 echo "Wait for tiller to move to the ready state before continuing"
