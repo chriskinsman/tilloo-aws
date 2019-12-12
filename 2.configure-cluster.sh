bcf7578815560f35f4724e5b4e450ac8ccadefba
Chris Kinsman <chris@kinsman.net>
2019-12-12 10:40:29 -0800

    Connect kubectl to cluster

--- 2.configure-cluster.sh
+++ 2.configure-cluster.sh
@@ -1,3 +1,4 @@
+aws eks update-kubeconfig --name tilloo --profile personal --region us-west-2
 kubectl apply -f config_aws_auth.yaml
 cp node-local-dns.yaml node-local-dns-temp.yaml
 export CLUSTER_IP=$(kubectl get services/kube-dns -n kube-system -o jsonpath="{.spec.clusterIP}")
